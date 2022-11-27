759703fbe9464aaaed50606800609139688eea18
Felix Blanke <felixblanke@uni-bonn.de>
2022-11-27 14:30:54 +0100

    Use product instead of nested for loops

--- pulp_solve.py
+++ pulp_solve.py
@@ -98,8 +98,7 @@ for timeslot_id in timeslot_ids:
         affine_constraint = lpSum(
             [
                 dec_vars[ak_id][timeslot_id][room_id][participant_id]
-                for ak_id in ak_ids
-                for room_id in room_ids
+                for ak_id, room_id in product(ak_ids, room_ids)
             ]
         )
         prob += affine_constraint <= 1, _construct_constraint_name(
@@ -111,8 +110,7 @@ for ak_id in ak_ids:
     affine_constraint = lpSum(
         [
             dec_vars[ak_id][timeslot_id][room_id][get_dummy_id(ak_id)]
-            for timeslot_id in timeslot_ids
-            for room_id in room_ids
+            for timeslot_id, room_id in product(timeslot_ids, room_ids)
         ]
     )
     prob += affine_constraint == aks[ak_id]["duration"], _construct_constraint_name(
@@ -120,141 +118,127 @@ for ak_id in ak_ids:
     )
 
 # for all A, P \neq P_A: \frac{1}{S_A} \sum_{Z, R} Y_{A, Z, R, P} <= 1
-for ak_id in ak_ids:
-    for participant_id in participant_ids:
-        if is_dummy(participant_id):
-            continue
-        affine_constraint = lpSum(
-            [
-                dec_vars[ak_id][timeslot_id][room_id][participant_id]
-                for ak_id in ak_ids
-                for room_id in room_ids
-            ]
-        )
-        for pref in preferences_dict[participant_id]:
-            if pref["ak_id"] == ak_id:
-                if pref["required"]:
-                    prob += (
-                        affine_constraint == aks[ak_id]["duration"],
-                        _construct_constraint_name(
-                            "PersonNeededForAK",
-                            ak_id,
-                            participant_id,
-                        ),
-                    )  ## TODO Check for fixed value
-                else:
-                    affine_constraint *= 1 / aks[ak_id]["duration"]
-                    prob += affine_constraint <= 1, _construct_constraint_name(
-                        "NoPartialParticipation",
+for ak_id, participant_id in product(ak_ids, participant_ids):
+    if is_dummy(participant_id):
+        continue
+    affine_constraint = lpSum(
+        [
+            dec_vars[ak_id][timeslot_id][room_id][participant_id]
+            for ak_id, room_id in product(ak_id, room_ids)
+        ]
+    )
+    for pref in preferences_dict[participant_id]:
+        if pref["ak_id"] == ak_id:
+            if pref["required"]:
+                prob += (
+                    affine_constraint == aks[ak_id]["duration"],
+                    _construct_constraint_name(
+                        "PersonNeededForAK",
                         ak_id,
                         participant_id,
-                    )
-                break
-        else:
-            affine_constraint *= 1 / aks[ak_id]["duration"]
-            prob += affine_constraint <= 1, _construct_constraint_name(
-                "NoPartialParticipation",
-                ak_id,
-                participant_id,
-            )
+                    ),
+                )  ## TODO Check for fixed value
+            else:
+                affine_constraint *= 1 / aks[ak_id]["duration"]
+                prob += affine_constraint <= 1, _construct_constraint_name(
+                    "NoPartialParticipation",
+                    ak_id,
+                    participant_id,
+                )
+            break
+    else:
+        affine_constraint *= 1 / aks[ak_id]["duration"]
+        prob += affine_constraint <= 1, _construct_constraint_name(
+            "NoPartialParticipation",
+            ak_id,
+            participant_id,
+        )
 
 
 # for all A, R: \sum_{Z} Y_{A, Z, R, P_A} <= 1
-for ak_id in ak_ids:
-    for room_id in room_ids:
-        affine_constraint = lpSum(
-            [
-                dec_vars[ak_id][timeslot_id][room_id][get_dummy_id(ak_id)]
-                for timeslot_id in timeslot_ids
-            ]
-        )
-        affine_constraint *= 1 / aks[ak_id]["duration"]
-        prob += affine_constraint <= 1, _construct_constraint_name(
-            "FixedAKRooms", ak_id, room_id
-        )
+for ak_id, room_id in product(ak_ids, room_ids):
+    affine_constraint = lpSum(
+        [
+            dec_vars[ak_id][timeslot_id][room_id][get_dummy_id(ak_id)]
+            for timeslot_id in timeslot_ids
+        ]
+    )
+    affine_constraint *= 1 / aks[ak_id]["duration"]
+    prob += affine_constraint <= 1, _construct_constraint_name(
+        "FixedAKRooms", ak_id, room_id
+    )
 
 # Ein AK findet konsekutiv statt:
-for ak_id in ak_ids:
-    for room_id in room_ids:
-        for timeslot_id_a, timeslot_id_b in combinations(timeslot_ids, 2):
-            block_idx_a, slot_in_block_idx_a = get_block_based_idx(timeslot_id_a)
-            block_idx_b, slot_in_block_idx_b = get_block_based_idx(timeslot_id_b)
+for ak_id, room_id in product(ak_ids, room_ids):
+    for timeslot_id_a, timeslot_id_b in combinations(timeslot_ids, 2):
+        block_idx_a, slot_in_block_idx_a = get_block_based_idx(timeslot_id_a)
+        block_idx_b, slot_in_block_idx_b = get_block_based_idx(timeslot_id_b)
 
-            if (
-                block_idx_a != block_idx_b
-                or abs(slot_in_block_idx_a - slot_in_block_idx_b)
-                >= aks[ak_id]["duration"]
-            ):
-                affine_constraint = lpSum(
-                    [
-                        dec_vars[ak_id, timeslot_id_a, room_id, get_dummy_id(ak_id)],
-                        dec_vars[ak_id, timeslot_id_b, room_id, get_dummy_id(ak_id)],
-                    ]
-                )
-                prob += affine_constraint <= 1, _construct_constraint_name(
-                    "AKConsecutive", ak_id, room_id, timeslot_id_a, timeslot_id_b
-                )
+        if (
+            block_idx_a != block_idx_b
+            or abs(slot_in_block_idx_a - slot_in_block_idx_b)
+            >= aks[ak_id]["duration"]
+        ):
+            affine_constraint = lpSum(
+                [
+                    dec_vars[ak_id, timeslot_id_a, room_id, get_dummy_id(ak_id)],
+                    dec_vars[ak_id, timeslot_id_b, room_id, get_dummy_id(ak_id)],
+                ]
+            )
+            prob += affine_constraint <= 1, _construct_constraint_name(
+                "AKConsecutive", ak_id, room_id, timeslot_id_a, timeslot_id_b
+            )
 
 # for all A, Z, R, P\neq P_A: 0 <= Y_{A, Z, R, P_A} - Y_{A, Z, R, P}
-for ak_id in ak_ids:
-    for timeslot_id in timeslot_ids:
-        for room_id in room_ids:
-            for participant_id in participant_ids:
-                affine_constraint = LpAffineExpression(
-                    dec_vars[ak_id][timeslot_id][room_id][get_dummy_id(ak_id)]
-                )
-                affine_constraint -= LpAffineExpression(
-                    dec_vars[ak_id][timeslot_id][room_id][participant_id]
-                )
-                prob += affine_constraint >= 0, "PersonVisitingAKAtRightTimeAndRoom"
+for ak_id, timeslot_id, room_id, participant_id in product(ak_ids, timeslot_ids, room_ids, participant_ids):
+    affine_constraint = LpAffineExpression(
+        dec_vars[ak_id][timeslot_id][room_id][get_dummy_id(ak_id)]
+    )
+    affine_constraint -= LpAffineExpression(
+        dec_vars[ak_id][timeslot_id][room_id][participant_id]
+    )
+    prob += affine_constraint >= 0, "PersonVisitingAKAtRightTimeAndRoom"
 
 # for all R, Z: \sum_{A, P\neq P_A} Y_{A, Z, R, P} <= K_R
-for room_id in room_ids:
-    for timeslot_id in timeslot_ids:
-        affine_constraint = lpSum(
-            [
-                dec_vars[ak_id][timeslot_id][room_id][participant_id]
-                for ak_id in ak_ids
-                for participant_id in participant_ids
-                if not is_dummy(participant_id)
-            ]
-        )
-        prob += affine_constraint <= rooms[room_id]["capacity"], "Roomsizes"
+for room_id, timeslot_id in product(room_ids, timeslot_ids):
+    affine_constraint = lpSum(
+        [
+            dec_vars[ak_id][timeslot_id][room_id][participant_id]
+            for ak_id, participant_id in product(ak_ids, participant_ids)
+            if not is_dummy(participant_id)
+        ]
+    )
+    prob += affine_constraint <= rooms[room_id]["capacity"], "Roomsizes"
 
 # for all Z, R, A'\neq A: Y_{A', Z, R, P_A} = 0
-for timeslot_id in timeslot_ids:
-    for room_id in room_ids:
-        for ak_id in ak_ids:
-            for dummy_ak_id in ak_ids:
-                if ak_id == dummy_ak_id:
-                    continue
-                _set_decision_variable(
-                    prob,
-                    ak_id,
-                    timeslot_id,
-                    room_id,
-                    get_dummy_id(dummy_ak_id),
-                    value=0,
-                    name="DummyPersonOneAk",
-                )
+for timeslot_id, room_id, ak_id, dummy_ak_id in product(timeslot_ids, room_ids, ak_ids, ak_ids):
+    if ak_id == dummy_ak_id:
+        continue
+    _set_decision_variable(
+        prob,
+        ak_id,
+        timeslot_id,
+        room_id,
+        get_dummy_id(dummy_ak_id),
+        value=0,
+        name="DummyPersonOneAk",
+    )
 
 # If P_{P, A} = 0: Y_{A,*,*,P} = 0 (non-dummy P)
 for participant_id, preferences in preferences_dict.items():
     if is_dummy(participant_id):
         continue
     pref_aks = {pref["ak_id"] for pref in preferences}
-    for ak_id in ak_ids.difference(pref_aks):
-        for timeslot_id in timeslot_ids:
-            for room_id in room_ids:
-                _set_decision_variable(
-                    prob,
-                    ak_id,
-                    timeslot_id,
-                    room_id,
-                    participant_id,
-                    value=0,
-                    name="PersonNotInterestedInAK",
-                )
+    for ak_id, timeslot_id, room_id in product(ak_ids.difference(pref_aks), timeslot_ids, room_ids):
+        _set_decision_variable(
+            prob,
+            ak_id,
+            timeslot_id,
+            room_id,
+            participant_id,
+            value=0,
+            name="PersonNotInterestedInAK",
+        )
 
 for participant_id in participant_ids:
     if is_dummy(participant_id):
@@ -263,84 +247,78 @@ for participant_id in participant_ids:
         if participant_time_constraint_dict[participant_id].difference(
             fulfilled_time_constraints[timeslot_id]
         ):
-            for ak_id in ak_ids:
-                for room_id in room_ids:
-                    _set_decision_variable(
-                        prob,
-                        ak_id,
-                        timeslot_id,
-                        room_id,
-                        participant_id,
-                        value=0,
-                        name="TimeImpossipleForPerson",
-                    )
+            for ak_id, room_id in product(ak_ids, room_ids):
+                _set_decision_variable(
+                    prob,
+                    ak_id,
+                    timeslot_id,
+                    room_id,
+                    participant_id,
+                    value=0,
+                    name="TimeImpossipleForPerson",
+                )
 
     for room_id in room_ids:
         if participant_room_constraint_dict[participant_id].difference(
             fulfilled_room_constraints[room_id]
         ):
-            for ak_id in ak_ids:
-                for timeslot_id in timeslot_ids:
-                    _set_decision_variable(
-                        prob,
-                        ak_id,
-                        timeslot_id,
-                        room_id,
-                        participant_id,
-                        value=0,
-                        name="RoomImpossibleForPerson",
-                    )
+            for ak_id, timeslot_id in product(ak_ids, timeslot_ids):
+                _set_decision_variable(
+                    prob,
+                    ak_id,
+                    timeslot_id,
+                    room_id,
+                    participant_id,
+                    value=0,
+                    name="RoomImpossibleForPerson",
+                )
 
 for ak_id in ak_ids:
     for timeslot_id in timeslot_ids:
         if ak_time_constraint_dict[ak_id].difference(
             fulfilled_time_constraints[timeslot_id]
         ):
-            for participant_id in participant_ids:
-                for room_id in room_ids:
-                    _set_decision_variable(
-                        prob,
-                        ak_id,
-                        timeslot_id,
-                        room_id,
-                        participant_id,
-                        value=0,
-                        name="TimeImpossipleForAK",
-                    )
+            for participant_id, room_id in product(participant_ids, room_ids):
+                _set_decision_variable(
+                    prob,
+                    ak_id,
+                    timeslot_id,
+                    room_id,
+                    participant_id,
+                    value=0,
+                    name="TimeImpossipleForAK",
+                )
 
     for room_id in room_ids:
         if ak_room_constraint_dict[ak_id].difference(
             fulfilled_room_constraints[room_id]
         ):
-            for participant_id in participant_ids:
-                for timeslot_id in timeslot_ids:
-                    _set_decision_variable(
-                        prob,
-                        ak_id,
-                        timeslot_id,
-                        room_id,
-                        participant_id,
-                        value=0,
-                        name="RoomImpossibleForPerson",
-                    )
+            for participant_id, timeslot_id in participant_ids, timeslot_ids:
+                _set_decision_variable(
+                    prob,
+                    ak_id,
+                    timeslot_id,
+                    room_id,
+                    participant_id,
+                    value=0,
+                    name="RoomImpossibleForPerson",
+                )
 
 
-for room_id in room_ids:
-    for timeslot_id in timeslot_ids:
-        if room_time_constraint_dict[room_id].difference(
-            fulfilled_time_constraints[timeslot_id]
-        ):
-            for participant_id in participant_ids:
-                for ak_id in ak_ids:
-                    _set_decision_variable(
-                        prob,
-                        ak_id,
-                        timeslot_id,
-                        room_id,
-                        participant_id,
-                        value=0,
-                        name="TimeImpossibleForRoom",
-                    )
+for room_id, timeslot_id in product(room_ids, timeslot_ids):
+    if room_time_constraint_dict[room_id].difference(
+        fulfilled_time_constraints[timeslot_id]
+    ):
+        for participant_id, ak_id in product(participant_ids, ak_ids):
+            _set_decision_variable(
+                prob,
+                ak_id,
+                timeslot_id,
+                room_id,
+                participant_id,
+                value=0,
+                name="TimeImpossibleForRoom",
+            )
 
 
 ## TODO: Set intitial value for eq constraints
