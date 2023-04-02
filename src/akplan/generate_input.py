1c5679c707e75867d3a0bbcbe72f4f20bbc5a243
Felix Blanke <felixblanke@uni-bonn.de>
2023-04-03 00:04:12 +0200

    Rework generate

--- src/akplan/generate_input.py
+++ src/akplan/generate_input.py
@@ -1,13 +1,22 @@
-import json
-import random
+"""Generate test inputs."""
 import argparse
+import json
+from collections import defaultdict
+
+import numpy as np
 
 
 def generate():
     parser = argparse.ArgumentParser()
     parser.add_argument("--persons", type=int, default=30)
     parser.add_argument("--aks", type=int, default=10)
+    parser.add_argument("--rooms", type=int, default=4)
+    parser.add_argument("--num_room_constraints", type=int, default=2)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--room_poisson_mean", type=float, default=1)
     args = parser.parse_args()
+
+    rng = np.random.default_rng(seed=args.seed)
 
     # we have one hour time slots
     # on Tuesday we go from 8-18, Wednesday from 8-16, Thursday from 8-16, Friday from 8-18
@@ -46,94 +55,113 @@ def generate():
         "blocks": list_of_time_blocks,
     }
 
+    all_room_constraints = [
+        f"room-constraint-{idx}" for idx in range(args.num_room_constraints)
+    ]
     # create rooms:
-    room1 = {
-        "id": "1",
-        "info": {"name": "Raum1"},
-        "capacity": 25,
-        "fulfilled_room_constraints": ["barrierefrei"],
-        "time_constraints": [],
-    }
-    room2 = {
-        "id": "2",
-        "info": {"name": "Raum2"},
-        "capacity": 30,
-        "fulfilled_room_constraints": ["barrierefrei", "Beamer"],
-        "time_constraints": [],
-    }
-    room3 = {
-        "id": "3",
-        "info": {"name": "Raum3"},
-        "capacity": 20,
-        "fulfilled_room_constraints": [],
-        "time_constraints": [],
-    }
-    room4 = {
-        "id": "4",
-        "info": {"name": "Raum4"},
-        "capacity": 25,
-        "fulfilled_room_constraints": ["Beamer", "barrierefrei"],
-        "time_constraints": [],
-    }
+    rooms = [
+        {
+            "id": str(room_idx),
+            "info": {"name": f"room {room_idx}"},
+            "capacity": rng.integers(low=10, high=51),
+            "fulfilled_room_constraints": rng.choice(
+                all_room_constraints,
+                size=rng.integers(low=0, high=len(all_room_constraints) + 1),
+                replace=False,
+            ),
+            "time_constraints": [],
+        }
+        for room_idx in range(args.rooms)
+    ]
 
     # create aks
-    list_of_aks = []
-    for index in range(args.aks):
-        # random number 1 or 2 for the duration
-        duration = random.choice([1, 2])
-        # with probability 10% the Ak needs a Beamer, with probability 90%, the AK doesn't
-        room_constraints = random.choices([[], ["Beamer"]], weights=[0.9, 0.1], k=1)[0]
-        # with probability 20% the AK is a ResoAK
-        is_reso_ak = random.choices([True, False], weights=[0.2, 0.8], k=1)[0]
-        time_constraints = ["ResoAK"] if is_reso_ak else []
+    room_constraint_arr = rng.choice(
+        all_room_constraints,
+        replace=False,
+        size=np.minimum(
+            len(all_room_constraints),
+            rng.poisson(lam=args.room_poisson_mean, size=args.aks),
+        ),
+    )
+    reso_ak_arr = rng.choice(2, p=[0.8, 0.2], size=args.aks).astype(bool)
+    duration_arr = rng.choice(2, size=args.aks) + 1
 
-        # write down the ak
-        ak = {
-            "id": str(index),
+    aks = [
+        {
+            "id": str(idx),
             "duration": duration,
             "properties": [],
             "room_constraints": room_constraints,
-            "time_constraints": time_constraints,
+            "time_constraints": ["ResoAK"] if is_reso_ak else [],
             "info": {
-                "name": "Name of an AK",
-                "head": "Name of the head of the AK",
-                "description": "Short description of the AK",
+                "name": f"AK {idx}",
+                "head": "N/A",
+                "description": "N/A",
                 "reso": is_reso_ak,
             },
         }
-        list_of_aks.append(ak)
+        for idx, (room_constraints, is_reso_ak, duration) in enumerate(
+            zip(room_constraint_arr, reso_ak_arr, duration_arr, strict=True)
+        )
+    ]
 
-    list_of_participants = []
-    # create participants
-    for index in range(args.persons):
-        preferences = []
-        # generate preferences for each AK:
-        for ak_index in range(args.aks):
-            is_required = random.choices([True, False], weights=[0, 1], k=1)[0]
-            preference_score = -1 if is_required else random.choice([0, 1, 2])
+    num_preferences_arr = np.minimum(
+        rng.poisson(
+            lam=max(10, round(0.2 * args.aks)),
+            size=args.persons,
+        ),
+        args.aks,
+    )
 
-            if preference_score != 0:
-                ak_preferences = {
-                    "ak_id": str(ak_index),
-                    "required": is_required,
-                    "preference_score": preference_score,
+    sampled_aks = {
+        person_idx: set(rng.choice(args.aks, replace=False, size=num_prefs))
+        for person_idx, num_prefs in enumerate(num_preferences_arr)
+    }
+
+    # 1. Ignore one half of participants
+    required_indices = rng.choice(
+        args.persons, size=round(0.5 * args.persons), replace=False
+    )
+    # 2. For each ak sample the person(s) required for the ak (0.1/0.8/0.1 split)
+    num_persons_required = rng.choice(3, size=args.aks, p=[0.1, 0.8, 0.1])
+
+    required_aks = defaultdict(set)
+    for ak_idx, num_required in enumerate(num_persons_required):
+        persons_required_for_ak = rng.choice(
+            required_indices, replace=False, size=num_required
+        )
+        for person_idx in persons_required_for_ak:
+            required_aks[person_idx].add(ak_idx)
+
+    def _calc_preferred_score(person_idx: int, ak_idx: int) -> int:
+        if ak_idx in required_aks[person_idx]:
+            return -1
+        return rng.choice(2) + 1
+
+    # TODO: Generate room & time constraints
+    participants = [
+        {
+            "id": str(person_idx),
+            "info": {"name": f"Person {person_idx}"},
+            "preferences": [
+                {
+                    "ak_id": ak_idx,
+                    "required": ak_idx in required_aks[person_idx],
+                    "preference_score": _calc_preferred_score(person_idx, ak_idx),
                 }
-                preferences.append(ak_preferences)
-
-        participant = {
-            "id": str(index),
-            "info": {"name": "Name of the Person"},
-            "preferences": preferences,
+                for ak_idx in preferred_aks.union(required_aks[person_idx])
+            ],
             "room_constraints": [],
             "time_constraints": [],
         }
-        list_of_participants.append(participant)
+        for person_idx, preferred_aks in sampled_aks.items()
+    ]
 
     # create dictionary that we later write into the json-file
     dictionary = {
-        "aks": list_of_aks,
-        "rooms": [room1, room2, room3, room4],
-        "participants": list_of_participants,
+        "aks": aks,
+        "rooms": rooms,
+        "participants": participants,
         "timeslots": time_slot_dictionary,
         "info": "DummySet",
     }
