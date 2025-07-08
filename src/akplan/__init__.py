a9f9c49b791314f9bc9bccbb74e6ecdd96a3a00c
Felix Blanke <info@fblanke.de>
2025-07-08 23:57:35 +0200

    Patch linopy's Highs solver to listen to user interrupts.

--- src/akplan/__init__.py
+++ src/akplan/__init__.py
@@ -8,4 +8,6 @@ __version__ = version("akplan")
 # by user interrupt not as an 'ok' solver state, so the solution values
 # are discarded. We patch this by trying to read a solution after a
 # user interrupt.
+# Note: We also patch linopy's Highs solver class to make the Highs
+# solver listen to keyboard interrupts.
 import akplan.monkeypatch_linopy  # noqa: F401
--- src/akplan/monkeypatch_linopy.py
+++ src/akplan/monkeypatch_linopy.py
@@ -1,17 +1,26 @@
-"""Monkey patches to the linopy library."""
+"""Monkey patches to the linopy library.
+
+This patches the linopy library in two ways:
+1. After a user interrupt, we attempt to read a solution
+   and if we suceed, we consider the solution status to be ok.
+2. We make the HiGHS solver listen to keyboard interrupts.
+"""
 
 from __future__ import annotations
 
 import logging
 from collections.abc import Callable
 from functools import partialmethod, update_wrapper
-from typing import TYPE_CHECKING
+from pathlib import Path
+from typing import TYPE_CHECKING, Protocol
 
 from linopy.constants import Solution, SolverStatus, TerminationCondition
-from linopy.solvers import Solver
+from linopy.solvers import Highs, Solver
 
 if TYPE_CHECKING:
-    from linopy.constants import Status
+    import highspy
+    from linopy.constants import Result, Status
+    from linopy.model import Model
 
 
 logger = logging.getLogger(__name__)
@@ -68,7 +77,57 @@ def safe_get_solution(
             logger.warning("Solution parsed successfully.")
             return sol
         except Exception as e:
-            logger.error(f"Failed to parse solution: {e}")
+            logger.error("Failed to parse solution: %s", e)
             return Solution()
 
     return unpatched_method(solver, status, func)
+
+
+class _SolverFunc(Protocol):
+    """Protocol to type hint the Highs._solve method."""
+
+    def __call__(
+        self,
+        solver: Highs,
+        h: highspy.Highs,
+        solution_fn: Path | None = None,
+        log_fn: Path | None = None,
+        warmstart_fn: Path | None = None,
+        basis_fn: Path | None = None,
+        model: Model | None = None,
+        io_api: str | None = None,
+        sense: str | None = None,
+    ) -> Result: ...
+
+
+@monkey_patch(Highs, pass_unpatched_method=True)
+def _solve(
+    solver: Highs,
+    h: highspy.Highs,
+    solution_fn: Path | None = None,
+    log_fn: Path | None = None,
+    warmstart_fn: Path | None = None,
+    basis_fn: Path | None = None,
+    model: Model | None = None,
+    io_api: str | None = None,
+    sense: str | None = None,
+    *,
+    unpatched_method: _SolverFunc,
+) -> Result:
+    """Patch `_solve` to make HiGHS listen for keyboard interrupts."""
+    # patch to use the highspy Model's solve function as the entry point
+    # `run` falls back to the C version, which skips the python code
+    # which allows to listen for keyboard interrupts.
+    h.run = h.solve  # type: ignore[method-assign]
+    h.HandleKeyboardInterrupt = True
+    return unpatched_method(
+        solver,
+        h,
+        solution_fn=solution_fn,
+        log_fn=log_fn,
+        warmstart_fn=warmstart_fn,
+        basis_fn=basis_fn,
+        model=model,
+        io_api=io_api,
+        sense=sense,
+    )
